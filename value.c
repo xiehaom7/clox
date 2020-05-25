d91d2cbbf3575368c253ca2c7a1acd2acd9c55ad
Hao Xie <xie@axonsoft.com>
2020-05-24 19:04:12 -0600

    Added to use NAN boxing to represent values

--- value.c
+++ value.c
@@ -30,6 +30,17 @@ void writeValueArray(ValueArray* array, Value value) {
 }
 
 void printValue(Value value) {
+#ifdef NAN_BOXING
+  if (IS_BOOL(value)) {
+    printf(AS_BOOL(value) ? "true" : "false");
+  } else if (IS_NIL(value)) {
+    printf("nil");
+  } else if (IS_NUMBER(value)) {
+    printf("%g", AS_NUMBER(value));
+  } else if (IS_OBJ(value)) {
+    printObject(value);
+  }
+#else
   switch (value.type) {
     case VAL_BOOL:
       printf(AS_BOOL(value) ? "true" : "false");
@@ -44,9 +55,14 @@ void printValue(Value value) {
       printObject(value);
       break;
   }
+#endif
 }
 
 bool valuesEqual(Value a, Value b) {
+#ifdef NAN_BOXING
+  if (IS_NUMBER(a) && IS_NUMBER(b)) return AS_NUMBER(a) == AS_NUMBER(b);
+  return a == b;
+#else
   if (a.type != b.type) return false;
 
   switch (a.type) {
@@ -60,4 +76,5 @@ bool valuesEqual(Value a, Value b) {
       return AS_OBJ(a) == AS_OBJ(b);
   }
   return false;
+#endif
 }
--- value.h
+++ value.h
@@ -6,12 +6,52 @@
 typedef struct sObj Obj;
 typedef struct sObjString ObjString;
 
-typedef enum {
-  VAL_BOOL,
-  VAL_NIL,
-  VAL_NUMBER,
-  VAL_OBJ
-} ValueType;
+#ifdef NAN_BOXING
+
+#define SIGN_BIT ((uint64_t)0x8000000000000000)
+#define QNAN ((uint64_t)0x7ffc000000000000)
+
+#define TAG_NIL 1    // 01.
+#define TAG_FALSE 2  // 10.
+#define TAG_TRUE 3   // 11.
+
+typedef uint64_t Value;
+
+#define IS_BOOL(v) (((v)&FALSE_VAL) == FALSE_VAL)
+#define IS_NIL(v) ((v) == NIL_VAL)
+#define IS_NUMBER(v) (((v)&QNAN) != QNAN)
+#define IS_OBJ(v) (((v) & (QNAN | SIGN_BIT)) == (QNAN | SIGN_BIT))
+
+#define AS_BOOL(v) ((v) == TRUE_VAL)
+#define AS_NUMBER(v) valueToNum(v)
+#define AS_OBJ(v) ((Obj*)(uintptr_t)((v) & ~(SIGN_BIT | QNAN)))
+
+#define BOOL_VAL(b) ((b) ? TRUE_VAL : FALSE_VAL)
+#define FALSE_VAL ((Value)(uint64_t)(QNAN | TAG_FALSE))
+#define TRUE_VAL ((Value)(uint64_t)(QNAN | TAG_TRUE))
+#define NIL_VAL ((Value)(uint64_t)(QNAN | TAG_NIL))
+#define NUMBER_VAL(num) numToValue(num)
+#define OBJ_VAL(obj) (Value)(SIGN_BIT | QNAN | (uint64_t)(uintptr_t)(obj))
+
+typedef union {
+  uint64_t bits;
+  double num;
+} DoubleUnion;
+
+static inline double valueToNum(Value value) {
+  DoubleUnion data;
+  data.bits = value;
+  return data.num;
+}
+
+static inline Value numToValue(double num) {
+  DoubleUnion data;
+  data.num = num;
+  return data.bits;
+}
+
+#else
+typedef enum { VAL_BOOL, VAL_NIL, VAL_NUMBER, VAL_OBJ } ValueType;
 
 typedef struct {
   ValueType type;
@@ -22,19 +62,21 @@ typedef struct {
   } as;
 } Value;
 
-#define IS_BOOL(value)    ((value).type == VAL_BOOL)
-#define IS_NIL(value)     ((value).type == VAL_NIL)
-#define IS_NUMBER(value)  ((value).type == VAL_NUMBER)
-#define IS_OBJ(value)     ((value).type == VAL_OBJ)
+#define IS_BOOL(value) ((value).type == VAL_BOOL)
+#define IS_NIL(value) ((value).type == VAL_NIL)
+#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
+#define IS_OBJ(value) ((value).type == VAL_OBJ)
 
-#define AS_BOOL(value)    ((value).as.boolean)
-#define AS_NUMBER(value)  ((value).as.number)
-#define AS_OBJ(value)     ((value).as.obj)
+#define AS_BOOL(value) ((value).as.boolean)
+#define AS_NUMBER(value) ((value).as.number)
+#define AS_OBJ(value) ((value).as.obj)
 
-#define BOOL_VAL(value)   ((Value){ VAL_BOOL, { .boolean = value } })
-#define NIL_VAL           ((Value){ VAL_NIL, { .number = 0 } })
-#define NUMBER_VAL(value) ((Value){ VAL_NUMBER, { .number = value } })
-#define OBJ_VAL(object)   ((Value){ VAL_OBJ, { .obj = (Obj*)object } })
+#define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+#define OBJ_VAL(object) ((Value){VAL_OBJ, {.obj = (Obj*)object}})
+
+#endif
 
 typedef struct {
   int capacity;
